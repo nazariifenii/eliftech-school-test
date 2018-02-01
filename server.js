aabb8a968ef8163677ee601910a742279e5af217
nazariifenii <nfenii02@gmail.com>
2018-02-01 16:16:01 +0200

    Structured code and added coments with explanations

--- server.js
+++ server.js
@@ -1,40 +1,65 @@
-const https = require('https');
+var axios = require('axios');
 
-var parsedJson='';
-var expressions = '';
-var id = '';
+var uri = 'https://www.eliftech.com/school-task';
 
-https.get('https://www.eliftech.com/school-task', (res) => {
-    //   console.log('statusCode:', res.statusCode);
-    //   console.log('headers:', res.headers);
+//Recieving a responce from server
+axios.get(uri)
+    .then(function (response) {
+      console.log(response.data);
+      var expressions = (response.data).expressions;
+      var currentId = response.data.id;
 
-      res.on('data', (d) => {
-        //process.stdout.write(d);
-        parsedJson = JSON.parse(d);
-        expressions = parsedJson.expressions;
-        id = parsedJson.id;
-        calculateExpressions(expressions);
-        //console.log(expressions[0] + '\n\n\n' + parsedJson.expressions[0]);
-      }); 
-    
-    }).on('error', (e) => {
-      console.error(e);
+      //Calculation all expressions and getting results
+      var currentResults = calculateExpressions(expressions);
+      console.log(currentResults);
+
+      //Checking the results of calculations
+      checkData (currentId, currentResults);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 
+function checkData(id, results){
+
+// Sending results to a server
+  axios.post(uri, {
+    'id': id,
+    'results': results
+  })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
 function calculateExpressions(exps){
-        var calculatingResult = [];
+        var resultingArr = [];
         var tokens = '';
         
+        //Loop through all sets of expressions
          for (var i = 0; i < exps.length; i++)
          {
             var stack = [];
             tokens = exps[i].split(' ');
+
+            //Loop through all tokens(elements) of each expression
             for(var j = 0; j < tokens.length; j++){
+
+              //Checking if the token is "number"
+              //Pushing all "numbers" to stack, until get a sign
               if (!isNaN(tokens[j])){
                 stack.push(tokens[j]);
-            
               } 
+              
+              //When got a sign - polling two last values from stack,
+              //performing calculations depending on type of token,
+              //pushing back to stack
               else{
+
+                //Getting last two values using stack rules
                 var secondOperand = +stack.pop();
                 var firstOperand = +stack.pop();
 
@@ -63,7 +88,8 @@ function calculateExpressions(exps){
                 }
               }
             }
-            calculatingResult.push(stack);
+            //Pushing resulting vulue to array of results translating to int
+            resultingArr.push(+stack);
         }
-        console.log(calculatingResult);
+        return resultingArr;
 }
